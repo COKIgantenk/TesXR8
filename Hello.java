8eb29fc0e6611488cb1f6a897b1c105a3d8c36af
COKIgantenk <choqie_naufal_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:01:53 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+public class Hello {
+    public static void main(String[] args)  {   
+    
+
+        String nama= "Choqie";
+        String asal_Kota= "Malang";
+        int usia=15;
+        double beratBadan=51.02;
+        float tinggiBadan=163.0f;
+        char jenisKelamin='p';
+        boolean status = false; 
+
+        System.out.println("Nama:"+nama);
+        System.out.println("Usia:"+usia);
+        System.out.println("Berat Badan:"+beratBadan);
+        System.out.println("Tinggi Badan:"+tinggiBadan);
+        System.out.println("Jenis Kelamin:"+jenisKelamin);
+        System.out.println("Status:"+status);
+
+     }
+    
+    
+}
