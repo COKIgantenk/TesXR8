364f62cabb2d8a42e842d02118cc8851da096345
COKIgantenk <choqie_naufal_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 22:25:57 +0700

    Add files via upload

---
+++ BukuTester.java
@@ -0,0 +1,40 @@
+public class BukuTester {
+    public static void main(String[] args) {
+        Buku BerubahBaik = new Buku(1, "BerubahBaik", "Kakgem", 1999);
+        Buku TiketSurga = new Buku(2, "TiketSurga", "GusPri", 2000);
+        Buku MenujuKesuksesan = new Buku(3, "MenujuKesuksesan", "GUSMIFTAH", 2077);
+        Buku TobatItuMudah = new Buku(4, "TobatItuMudah", "Kurnaen", 1988);
+        Buku AlimUlama = new Buku(5, "AlimUlama", "lojiony", 1277);
+
+         //BerubahBaik
+          System.out.println(BerubahBaik.id);
+          System.out.println(BerubahBaik.judul);
+          System.out.println(BerubahBaik.pengarang);
+          System.out.println(BerubahBaik.Tahunterbit);
+
+        //TiketSurga
+          System.out.println(TiketSurga.id);
+          System.out.println(TiketSurga.judul);
+          System.out.println(TiketSurga.pengarang);
+          System.out.println(TiketSurga.Tahunterbit);
+
+        //MenujuKesuksesan
+          System.out.println(MenujuKesuksesan.id);  
+          System.out.println(MenujuKesuksesan.judul);  
+          System.out.println(MenujuKesuksesan.pengarang);  
+          System.out.println(MenujuKesuksesan.Tahunterbit);  
+
+        //TobatItuMudah
+          System.out.println(TobatItuMudah.id); 
+          System.out.println(TobatItuMudah.judul); 
+          System.out.println(TobatItuMudah.pengarang); 
+          System.out.println(TobatItuMudah.Tahunterbit); 
+
+        //AlimUlama
+          System.out.println(AlimUlama.id);  
+          System.out.println(AlimUlama.judul);  
+          System.out.println(AlimUlama.pengarang);  
+          System.out.println(AlimUlama.Tahunterbit);  
+
+    }
+}
